503d543448a589a4b3afe5123a1870b013fce257
Hri7566 <hri7566@gmail.com>
2023-09-19 10:34:13 -0400

    Coming soon

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-white bg-black min-h-screen flex">
+      <div className="grid content-center justify-center min-h-full w-full text-center bg-gradient-to-t from-black to-[#100000]">
+        <h1 className="text-5xl pb-5">Khorne Bot</h1>
+        <p className="text-xl">Coming Soon</p>
+      </div>
+    </main>
+  );
+}
